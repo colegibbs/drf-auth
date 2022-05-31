29d66d441961aae78101488c057af0a611b332d0
Cole Gibbs <colegibbs0@gmail.com>
2022-05-31 14:36:57 -0700

    imports, BookList class, BookDetail class"

--- books/views.py
+++ books/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import book
+from .permissions import IsOwnerOrReadOnly
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookList(generics.ListCreateAPIView):
+  queryset = book.objects.all()
+  serializer_class = BookSerializer
+
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+  permission_classes = (IsOwnerOrReadOnly,)
+  queryset = book.objects.all()
+  serializer_class = BookSerializer
