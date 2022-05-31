8c5a48a3368029cb53c387208133c394fa53ab1e
Cole Gibbs <colegibbs0@gmail.com>
2022-05-31 15:48:21 -0700

    added postgres database info

--- drf_api_permissions_postgres_project/settings.py
+++ drf_api_permissions_postgres_project/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'drf_api_permissions_postgres_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
 
